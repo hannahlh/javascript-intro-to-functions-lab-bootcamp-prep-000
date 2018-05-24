9068648d502b2260708f4cbf12a6f9da939d7919
Hannah Hughes <hugheshannah96@gmail.com>
2018-05-24 18:01:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-  return string.toUpperCase()
-  return string("I love you, too.")()
+  return string("I love you,Grandma."')()
 }
